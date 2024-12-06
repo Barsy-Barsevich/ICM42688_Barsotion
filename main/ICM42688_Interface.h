cfe02b98d3baf4b3ae04b363b9ff93100b330d86
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2024-12-06 21:59:01 +0500

    Accel & Gyro conversion error fixed

--- main/ICM42688_Interface.h
+++ main/ICM42688_Interface.h
@@ -13,3 +13,6 @@ void ICM42688_SPI_InterfaceInit(int miso, int mosi, int sck, int cs);
 
 void ICM42688_SPI_readRegister(uint8_t reg, uint8_t *buf);
 void ICM42688_SPI_writeRegister(uint8_t reg, uint8_t data);
+
+void ICM42688_I2C_readRegister(uint8_t reg, uint8_t *buf);
+void ICM42688_I2C_writeRegister(uint8_t reg, uint8_t data);
