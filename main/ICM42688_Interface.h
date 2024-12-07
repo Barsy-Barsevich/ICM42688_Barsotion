785e43751855285dff6ad3c67189fc3ba69ea335
Barsy-Barsevich <barsotion@yandex.ru>
2024-12-07 17:11:46 +0500

    Interrupt init added, not tested

--- main/ICM42688_Interface.h
+++ main/ICM42688_Interface.h
@@ -19,6 +19,7 @@ void ICM42688_SPI_InterfaceInit(spi_host_device_t host, int miso, int mosi, int 
 
 void ICM42688_SPI_readRegister(uint8_t reg, uint8_t *buf);
 void ICM42688_SPI_writeRegister(uint8_t reg, uint8_t data);
+void ICM42688_SPI_readFIFO(ICM42688_Interface_t *local, uint8_t *buf, size_t quan);
 
 void ICM42688_I2C_readRegister(uint8_t reg, uint8_t *buf);
 void ICM42688_I2C_writeRegister(uint8_t reg, uint8_t data);
