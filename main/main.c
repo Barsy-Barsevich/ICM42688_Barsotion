4dd56090895adba21b4d109be1cdb29a482b0a91
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2024-12-11 19:18:19 +0500

    FIFO-syncronous read

--- main/main.c
+++ main/main.c
@@ -27,24 +27,39 @@ void app_main(void)
 		.spi.mosi_pin = MOSI_PINNUM,
 		.spi.sck_pin = CLK_PINNUM,
 		.spi.cs_pin = CS_PINNUM,
-		.spi.sck_freq = 1000000,
+		.spi.sck_freq = 10000000,
 		.accel.enable = ENABLE_XA | ENABLE_YA | ENABLE_ZA,
 		.accel.mode = ACCEL_LN_MODE,
-		.accel.odr = ACCEL_ODR_6p25HZ,
+		.accel.odr = ACCEL_ODR_500HZ,
 		.accel.fs_sel = ACCEL_16G_COEF,
 		.gyro.enable = ENABLE_XG | ENABLE_YG | ENABLE_ZG,
 		.gyro.mode = GYRO_LN_MODE,
-		.gyro.odr = GYRO_ODR_12p5HZ,
+		.gyro.odr = GYRO_ODR_500HZ,
 		.gyro.fs_sel = GYRO_FS_SEL_2000DPS,
 		.fifo.mode = FIFO_STOP_ON_FULL_MODE,
-		.fifo.watermark = 0xFFF,
+		.fifo.watermark = 20,
 	};
     
     ICM42688_Init(&hicm, &icm_cfg);
 //    hicm.readRegister = ICM42688_SPI_readRegister;
 	//hicm.accel_coef = 16. / 32768.;
 	
-	static uint8_t buf[5000] = {0};
+	while (1)
+	{
+		while (!ICM42688_FIFO_THS_IRQ_Check(&hicm));
+		uint8_t dummy[2];
+		dummy[0] = 0;
+		hicm.readRegister(ICM_0_FIFO_COUNTH, dummy);
+		hicm.readRegister(ICM_0_FIFO_COUNTL, dummy+1);
+		printf("%u\t", (uint16_t)dummy[0]<<8 | dummy[1]);
+		int32_t raw[6];
+		ICM42688_readFIFO(&hicm, raw);
+		ICM42688_calculateAccel(&hicm, raw+3);
+		ICM42688_calculateGyro(&hicm, raw);
+		//printf("%ld, %ld, %ld, %f, %f, %f\n", raw[0], raw[1], raw[2], hicm.gyro.x, hicm.gyro.y, hicm.gyro.z);
+		printf("%f, %f, %f\n", hicm.gyro.x, hicm.gyro.y, hicm.gyro.z);
+		
+	}
     
     while (1)
     {
