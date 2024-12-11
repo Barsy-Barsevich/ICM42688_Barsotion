afc943386fd14c3e6cf96b5b736b899f094e2d75
Barsy-Barsevich <barsotion@yandex.ru>
2024-12-11 19:31:35 +0500

    Delete prev cycle

--- main/main.c
+++ main/main.c
@@ -30,11 +30,11 @@ void app_main(void)
 		.spi.sck_freq = 10000000,
 		.accel.enable = ENABLE_XA | ENABLE_YA | ENABLE_ZA,
 		.accel.mode = ACCEL_LN_MODE,
-		.accel.odr = ACCEL_ODR_500HZ,
+		.accel.odr = ACCEL_ODR_50HZ,
 		.accel.fs_sel = ACCEL_16G_COEF,
 		.gyro.enable = ENABLE_XG | ENABLE_YG | ENABLE_ZG,
 		.gyro.mode = GYRO_LN_MODE,
-		.gyro.odr = GYRO_ODR_500HZ,
+		.gyro.odr = GYRO_ODR_50HZ,
 		.gyro.fs_sel = GYRO_FS_SEL_2000DPS,
 		.fifo.mode = FIFO_STOP_ON_FULL_MODE,
 		.fifo.watermark = 20,
@@ -59,48 +59,5 @@ void app_main(void)
 		//printf("%ld, %ld, %ld, %f, %f, %f\n", raw[0], raw[1], raw[2], hicm.gyro.x, hicm.gyro.y, hicm.gyro.z);
 		printf("%f, %f, %f\n", hicm.gyro.x, hicm.gyro.y, hicm.gyro.z);
 		
-	}
-    
-    while (1)
-    {
-//		printf("Reading who_am_i:");
-//		int32_t raw[6];
-//		//hicm.read_reg(ICM_0_WHO_AM_I, &dummy);
-//		ICM42688_readRegAG(&hicm, raw);
-//		//printf(" %d, %d, %d, %d, %d, %d\n", (int)raw[0], (int)raw[1], (int)raw[2], (int)raw[3], (int)raw[4], (int)raw[5]);
-//		//printf(" %04lX, %ld\n", (uint32_t)raw[0], raw[0]);
-//		ICM42688_calculateGyro(&hicm, raw);
-//		ICM42688_calculateAccel(&hicm, raw+3);
-//		printf("%ld, %ld, %ld, %f, %f, %f\n", raw[3], raw[4], raw[5], hicm.accel.x, hicm.accel.y, hicm.accel.z);
-
-//		ICM42688_SPI_readFIFO(hicm.interface, buf, 20);
-//		
-//		for (size_t i=0; i<20; i++)
-//		{
-//			printf("%02X ", buf[i]);
-//		}
-//		printf("\n");
-
-		int32_t raw[6];
-		ICM42688_readFIFO(&hicm, raw);
-		ICM42688_calculateAccel(&hicm, raw+3);
-		ICM42688_calculateGyro(&hicm, raw);
-		printf("%ld, %ld, %ld, %f, %f, %f\n", raw[0], raw[1], raw[2], hicm.gyro.x, hicm.gyro.y, hicm.gyro.z);
-
-		uint8_t dummy[2];
-		dummy[0] = 0;
-		hicm.readRegister(ICM_0_FIFO_COUNTH, dummy);
-		hicm.readRegister(ICM_0_FIFO_COUNTL, dummy+1);
-		printf("Count: %u\t", (uint16_t)dummy[0]<<8 | dummy[1]);
-//		
-//		ICM42688_SPI_readFIFO(hicm.interface, buf, 20);
-//		
-//		hicm.readRegister(ICM_0_FIFO_LOST_PKT1, dummy);
-//		hicm.readRegister(ICM_0_FIFO_LOST_PKT0, dummy+1);
-//		printf("Lost: %u\n", (uint16_t)dummy[0]<<8 | dummy[1]);
-		
-		
-		
-		vTaskDelay(8);
 	}
 }
